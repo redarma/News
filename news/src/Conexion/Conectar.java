fbbb8e43401c2f8ecbc7a4d7bfb006fb528351cd
juanrt1909 <juanrt1909@gmail.com>
2014-06-25 17:25:57 -0500

    Create Conectar.java

---
+++ news/src/Conexion/Conectar.java
@@ -0,0 +1,39 @@
+package Conexion;
+import java.sql.*;
+public class Conectar {
+	
+	private static Connection conectar;
+	private static Conectar conectado = new Conectar();
+	private Statement s;
+	private ResultSet r;
+	
+	private Conectar(){}
+	
+	public static Conectar getConexionInstance(){
+        return conectado;
+    }
+	
+	public void conectar() throws SQLException{
+		DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+		conectar = DriverManager.getConnection ("jdbc:mysql://localhost/news","root", "Juan12345*");
+		s = conectar.createStatement();
+		System.out.println("Conección exitosa");
+	}
+	
+	public void cerrar() throws SQLException{
+		conectar.close();
+	}
+	
+	public ResultSet consulta(String sql) throws SQLException{
+		conectar();
+		r = s.executeQuery(sql);
+		cerrar();
+		return r; 
+	}
+	
+	public void actualizar(String sql) throws SQLException{
+		conectar();
+		s.executeUpdate(sql);
+		cerrar();
+	}
+}
