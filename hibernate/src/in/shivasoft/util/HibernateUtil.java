4184ce3425ae6b8a19e9629265d696c3b5144f47
Juan Ramirez Ticona <juanrt1909@gmail.com>
2014-07-16 23:06:51 -0500

    hibernate prueba inicial

---
+++ hibernate/src/in/shivasoft/util/HibernateUtil.java
@@ -0,0 +1,33 @@
+package in.shivasoft.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+@SuppressWarnings("deprecation")
+public class HibernateUtil {
+
+	private static final SessionFactory sessionFactory;
+
+	static {
+
+		try {
+
+			sessionFactory = new Configuration().configure()
+			.buildSessionFactory();
+
+		} catch (Throwable ex) {
+
+			System.err.println("Initial SessionFactory creation failed." + ex);
+
+			throw new ExceptionInInitializerError(ex);
+
+		}
+
+	}
+
+	public static SessionFactory getSessionFactory() {
+
+		return sessionFactory;
+
+	}
+}
